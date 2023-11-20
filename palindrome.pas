a34edbd0e0b515b982a8972c2430a176baece045
ahmedasee2022 <147337679+ahmedasee2022@users.noreply.github.com>
2023-11-20 19:20:51 +0200

    Add files via upload

--- palindrome.pas
+++ palindrome.pas
@@ -1,25 +1,32 @@
-program palindrome;
 function palindrome(word: string): boolean;
+var
+  i, j: integer;
 begin
-if lenght(word) <=1 then
-palindrome :=True // base case: single character string is palindrome
-else if word[1]<> word[lenght(word)]then
-palindrome :=False //first and last character don't match, not a polindrome
-else
-palindrome :=palindrome(copy(word, 2, lenght(word)-2)) //recur on the substring without first and last characters
-end;
-    
- var 
- word: string;
- begin
- write('enter a word: ');
- read (word)
+  i := 1; // index for the first character
+  j := length(word); // index for the last character
 
-if palindrome(word) then
-  write('The word "', word, '" is a palindrome.')
+  while i < j do
+  begin
+    if word[i] <> word[j] then
+    begin
+      palindrome := False; // characters don't match, not a palindrome
+    end;
+    i := i + 1; // increment the first index
+    j := j - 1; // decrement the last index
+  end;
+  palindrome := True; // all characters matched, it is a palindrome
+end;
+
+var
+  word: string;
+begin
+  write('Enter a word: ');
+  read(word); // Added 'ln' to read the newline character
+
+  if palindrome(word) then
+    write('The word "', word, '" is a palindrome.')
   else
     write('The word "', word, '" is not a palindrome.');
 
-  read;
-
+  read; // Added 'ln' to read the newline character
 end.
